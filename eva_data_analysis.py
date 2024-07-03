2482b32d3432ca4af1dc2f2b4b70ae6774465f6f
gbibbo <gabobibbo@gmail.com>
2024-07-03 12:57:16 +0100

    Readme added

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
-
+import re
 
 def read_json_to_dataframe(input_file_):
     """
@@ -94,6 +94,68 @@ def plot_cumulative_time_in_space(df_, graph_file_):
     plt.savefig(graph_file_)
     plt.show()
 
+def calculate_crew_size(crew):
+    """
+    Calculate crew_size for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column
+
+    Returns:
+        int: The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+
+def add_crew_size_variable(df_):
+    """
+    Add crew size (crew_size) variable to the dataset
+
+    Args:
+        df_ (pd.DataFrame): The input dataframe.
+
+    Returns:
+        pd.DataFrame: A copy of df_ with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df_.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
+
+
+def summarise_categorical(df_, varname_):
+    """
+    Tabulate the distribution of a categorical variable
+
+    Args:
+        df_ (pd.DataFrame): The input dataframe.
+        varname_ (str): The name of the variable
+
+    Returns:
+        pd.DataFrame: dataframe containing the count and percentage of
+        each unique value of varname_
+    """
+    print(f'Tabulating distribution of categorical variable {varname_}')
+
+    # Prepare statistical summary
+    count_variable = df_[[varname_]].copy()
+    count_summary = count_variable.value_counts() # There is a bug here that we will fix later!
+    percentage_summary = round(count_summary / count_variable.size, 2) * 100
+
+    # Combine results into a summary data frame
+    df_summary = pd.concat([count_summary, percentage_summary], axis=1)
+    df_summary.columns = ['count', 'percentage']
+    df_summary.sort_index(inplace=True)
+
+
+    df_summary = df_summary.reset_index()
+    return df_summary
+
 
 if __name__ == '__main__':
 
@@ -110,8 +172,14 @@ if __name__ == '__main__':
 
     eva_data = read_json_to_dataframe(input_file)
 
-    write_dataframe_to_csv(eva_data, output_file)
+    eva_data_prepared = add_crew_size_variable(eva_data)
 
-    plot_cumulative_time_in_space(eva_data, graph_file)
+    write_dataframe_to_csv(eva_data_prepared, output_file)
+
+    table_crew_size = summarise_categorical(eva_data_prepared, "crew_size")
+
+    write_dataframe_to_csv(table_crew_size, "./table_crew_size.csv")
+
+    plot_cumulative_time_in_space(eva_data_prepared, graph_file)
 
     print("--END--")
